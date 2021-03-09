4eceb7a85c24bf5bac7fd581e685c3bdff9d4c09
jpcorreia99 <jpcorreia99@gmail.com>
2021-03-09 00:07:47 +0000

    remove argument

--- Generator/include/Box.h
+++ Generator/include/Box.h
@@ -16,8 +16,8 @@ private:
     int width,depth,height,nDivisions;
     std::vector<Triangulo*> faces;
     std::map<std::tuple<int,int,int>,Ponto3D*> points;
-    void addYLayer(int iterations,bool top);
-    void addXLayer(int iterations,bool top);
-    void addZLayer(int iterations,bool top);
+    void addYLayer(bool top);
+    void addXLayer(bool top);
+    void addZLayer(bool top);
     void addSquare(bool top,Ponto3D * topRight,Ponto3D * topLeft,Ponto3D * bellowLeft,Ponto3D * bellowRight);
 };
